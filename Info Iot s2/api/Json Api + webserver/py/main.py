764466ff3c547099146feb0f4bb0b70a48152436
CruZxPog <andredacruzribeiro13@gmail.com>
2025-04-03 11:46:52 +0200

    Update main.py

--- Info Iot s2/api/Json Api + webserver/py/main.py
+++ Info Iot s2/api/Json Api + webserver/py/main.py
@@ -16,6 +16,7 @@
 from flask import Flask
 from flask import render_template
 import requests
+from pydantic import BaseModel
 # https://pypi.org/project/requests/
 
 app = Flask(__name__)
@@ -24,8 +25,32 @@ DATABASE_API_URL = "https://my-json-server.typicode.com/CruZxPog/json/guns"
 
 response = requests.get(DATABASE_API_URL)
 guns = response.json()
-print(guns)
 
-# @app.route("/")
-# def start_page():
-#     render_template("index.html",guns=gun_list)
+class gun(BaseModel):
+    id: int
+    name: str
+    category: str
+    weight: str
+    length: str
+    barrel_length: str
+    cartridge: str
+    action: str
+    muzzle_velocity: str
+    effective_range: str
+    magazine_capacity: str
+    fire_rate: str
+    img_url: str
+
+gun_list = []
+for gun_data in guns:
+    #https://www.geeksforgeeks.org/python-unpack-dictionary/
+    # ** is for unpacking the dictionary into keyword arguments
+    gun_list.append(gun(**gun_data))
+
+#print(gun_list)
+
+
+@app.route("/")
+def start_page():
+    gun_types = [gun.category for gun in gun_list]
+    render_template("index.html",guns=gun_types)
